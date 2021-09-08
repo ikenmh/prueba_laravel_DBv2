e83a3da59bccf6d5cf407d2761b2ad1ded892861
Elvys Nilton Mamani Herbas <emamani@eapbolivia.org>
2021-09-08 00:53:10 -0700

    Created Migrations

---
+++ database/migrations/2021_09_08_074845_create_endpoint_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEndpointTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('endpoint', function (Blueprint $table) {
+            $table->increments("endpoint_id");
+            $table->integer("rol_id")->unsigned();
+            $table->string("endpoint_name",100);
+            $table->string("route",200);
+            $table->foreign("rol_id")->references("rol_id")->on("rol");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('endpoint');
+    }
+}
